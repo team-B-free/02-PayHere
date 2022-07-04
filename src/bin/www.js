d8462387be52a9ffbdeca067570229b936a3209a
chldppwls12 <chldppwls12@naver.com>
2022-07-04 15:38:40 +0900

    [Chore] sequlize 초기 세팅

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,8 +1,16 @@
 import app from "../index.js";
 import { logger } from "../config/winston.js";
+import db from '../models/index.js';
 
 const PORT = process.env.PORT || 3000; // eslint-disable-line
 
-app.listen(PORT, () => {
-  logger.info(`${process.env.NODE_ENV} - API Server Start at Port ${PORT}`); // eslint-disable-line
-});
+db.sequelize.sync()
+.then(() => {
+  app.listen(PORT, () => {
+    logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.log(err);
+  logger.error(`${process.env.NODE_ENV} - DB connection Error`);
+  });
