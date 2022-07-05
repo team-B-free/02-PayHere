92eba1e7c1634c5611acb04b495f0436f012534f
chldppwls12 <chldppwls12@naver.com>
2022-07-05 09:38:47 +0900

    [Feat] DB 모델링

---
+++ src/models/comment.js
@@ -0,0 +1,29 @@
+import { DataTypes, Model } from 'sequelize';
+
+class Comment extends Model {
+  static init(sequelize){
+    return super.init({
+      content : {
+        type: DataTypes.STRING(300),
+        allowNull: false
+      },
+    },
+    {
+      sequelize,
+      modelName: "COMMENT",
+      tableName: "COMMENT",
+      charset: "utf8mb4", //이모지 가능
+      collate: "utf8mb4_general_ci",  //한글 저장되도록
+      freezeTableName: true,  //테이블명 이름 그대로 사용
+      timestamps: true,  //createdAt, updatedAt 컬럼 자동 추가
+      paranoid: true, //deletedAt 컬럼 자동 추가
+      underscored: true,  //컬럼명 스네이크 형식으로 변경
+    })
+  }
+
+  static associate(db){
+    db.Comment.belongsTo(db.Moneybook, {foreignKey: "moneybook_id"});
+  }
+}
+
+export default Comment;
---
+++ src/models/moneybook.js
@@ -0,0 +1,41 @@
+import { DataTypes, Model } from 'sequelize';
+
+class Moneybook extends Model{
+  static init(sequelize){
+    return super.init({
+      title : {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      is_shared : {
+        type: DataTypes.STRING(1),
+        allowNull: false,
+        defaultValue: 'N'
+      },
+      view: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+    },
+    {
+      sequelize,
+      modelName: "MONEYBOOK",
+      tableName: "MONEYBOOK",
+      charset: "utf8mb4", //이모지 가능
+      collate: "utf8mb4_general_ci",  //한글 저장되도록
+      freezeTableName: true,  //테이블명 이름 그대로 사용
+      timestamps: true,  //createdAt, updatedAt 컬럼 자동 추가
+      paranoid: true, //deletedAt 컬럼 자동 추가
+      underscored: true,  //컬럼명 스네이크 형식으로 변경
+    })
+  }
+
+  static associate(db){
+    db.Moneybook.hasMany(db.MoneybookDetail, {foreignKey: "moneybook_id"});
+    db.Moneybook.hasMany(db.Comment, {foreignKey: "moneybook_id"});
+    db.Moneybook.belongsTo(db.User, {foreignKey: "user_id"});
+  }
+}
+
+export default Moneybook;
