2f4d380a87d69f1eff4941362cc9db0dfc5f847f
chldppwls12 <chldppwls12@naver.com>
2022-07-06 15:01:57 +0900

    [Fix] #7 - 로그인 api 수정  - method 변경  - authJWT 미들웨어 사용x

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { validate } from 'express-validation';
 import userController from "./../../controllers/user/userController.js";
-import authJWT from '../../middlewares/auth.js';
 import * as validator from '../../validations/user/userValidator.js';
 
 const router = Router();
 
 router.get("/test", userController.getTest);
 router.get("/token-resign", validate(validator.resignToken), userController.resignToken);
-router.get("/login", validate(validator.login), authJWT, userController.login);
+router.post("/login", validate(validator.login), userController.login);
 
 export default router;
