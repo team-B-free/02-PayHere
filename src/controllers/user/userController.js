27a9499f2ed6232e2064284c9cd807cc745d9398
chldppwls12 <chldppwls12@naver.com>
2022-07-07 07:22:41 +0900

    [Refactor] #7 - 로그인 쪽 리팩토링  - 로직 Controller에서 Service로 이동

--- src/controllers/user/userController.js
+++ src/controllers/user/userController.js
@@ -1,9 +1,9 @@
-import { signTokens, resignAccessToken } from '../../utils/jwtUtil.js';
+import { resignAccessToken } from '../../utils/jwtUtil.js';
 import userService from '../../services/user/userService.js';
 import { response, errResponse } from '../../utils/response.js';
 import statusCode from '../../utils/statusCode.js';
 import message from '../../utils/responseMessage.js'
-import { loginResponse, resignTokenResponse } from '../../utils/responseData.js';
+import { resignTokenResponse } from '../../utils/responseData.js';
 import { refreshStatus } from '../../utils/constants.js';
 
 const userController = {
@@ -13,20 +13,11 @@ const userController = {
 
   login: async (req, res) => {
     const { email, password } = req.body;
-    
-    const userId = await userService.getUserId(email, password);
-    if (userId === null){
-      return res
-      .status(statusCode.BAD_REQUEST)
-      .send(errResponse(statusCode.BAD_REQUEST, message.INVALID_USER_INFO));
-    }
-
-    const { accessToken, refreshToken } = await signTokens(userId);
-    const data = loginResponse(accessToken, refreshToken);
+    const [statusCode, result] = await userService.login(email, password);
 
     return res
-      .status(statusCode.OK)
-      .send(response(statusCode.OK, message.SUCCESS, data));
+      .status(statusCode)
+      .send(result);
   },
 
   resignToken: async (req, res) => {
