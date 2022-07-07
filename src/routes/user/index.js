1feb0cbb78335fbf05ea18d2fd8f7c50f5427949
chldppwls12 <chldppwls12@naver.com>
2022-07-07 09:32:15 +0900

    [Fix] #7 토큰 재발급 api method 변경

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -6,7 +6,7 @@ import * as validator from '../../validations/user/userValidator.js';
 const router = Router();
 
 router.get("/test", userController.getTest);
-router.get("/token-resign", validate(validator.resignToken), userController.resignToken);
+router.post("/token-resign", validate(validator.resignToken), userController.resignToken);
 router.post("/login", validate(validator.login), userController.login);
 router.post("/signup", validate(validator.signUp), userController.signUp);
 
