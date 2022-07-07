14f94c2fa5a867fe645ed2841f4cba0efd6659b1
KCH6937 <adasddasd12@naver.com>
2022-07-07 15:38:43 +0900

    [Refactor] #17 - /user -> /users로 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import userRouter from "./user/index.js";
 import MoneybookRouter from "./moneybook/index.js";
 import MoneybookDetailRouter from "./moneybook_detail/index.js";
 
-router.use("/user", userRouter);
+router.use("/users", userRouter);
 router.use("/moneybook", MoneybookRouter);
 router.use("/moneybook", MoneybookRouter);
 router.use("/moneybook/:idx", MoneybookDetailRouter);
