57d7669620651ecc9089b44fb534e3a0fc7e0e83
KCH6937 <adasddasd12@naver.com>
2022-07-08 23:42:58 +0900

    [Refactor] Validate 라우터 추가

--- src/routes/moneybook/index.js
+++ src/routes/moneybook/index.js
@@ -1,12 +1,16 @@
-import { Router } from "express";
-import moneybookController from "./../../controllers/moneybook/moneybookController.js";
-import commentController from "./../../controllers/comment/commentController.js";
+import { Router } from 'express';
+import { validate } from 'express-validation';
+import moneybookController from './../../controllers/moneybook/moneybookController.js';
+import commentController from './../../controllers/comment/commentController.js';
+import * as commentValidator from '../../validations/comment/commentValidator.js';
+import * as moneybookValidator from '../../validations/moneybook/moneybookValidator.js';
+
 const router = Router();
 /**
  * @author 오주환
  * @version 1.0 22.07.06 가계부 수정
  */
-router.patch("/:moneybook_id", moneybookController.updateMoneybook);
+router.patch('/:moneybook_id', moneybookController.updateMoneybook);
 
 /**
  * @author 오주환
@@ -18,15 +22,39 @@ router.patch("/:moneybook_id", moneybookController.updateMoneybook);
  *  @author 박성용
  *  @version 1.2 22.7.7 {가계부 삭제기능 delete 메서드로 변경}
  */
-router.get("/", moneybookController.getMbtiTypeMoneybook);
-router.patch("/recover/:moneybook_id", moneybookController.setRestoreMoneyBook);
-router.delete("/:moneybook_id/status/", moneybookController.setDeleteMoneybook);
+router.get(
+  '/',
+  validate(moneybookValidator.getMbtiTypeMoneybook),
+  moneybookController.getMbtiTypeMoneybook,
+);
+router.patch(
+  '/recover/:moneybook_id',
+  validate(moneybookValidator.setRestoreMoneyBook),
+  moneybookController.setRestoreMoneyBook,
+);
+router.delete(
+  '/:moneybook_id/status/',
+  validate(moneybookValidator.setDeleteMoneybook),
+  moneybookController.setDeleteMoneybook,
+);
 
 //댓글 생성
-router.post("/comments", commentController.createComment);
+router.post(
+  '/comments',
+  validate(commentValidator.createComment),
+  commentController.createComment,
+);
 //댓글 수정
-router.patch("/comments/:comment_id", commentController.updateComment);
+router.patch(
+  '/comments/:comment_id',
+  validate(commentValidator.updateComment),
+  commentController.updateComment,
+);
 //댓글 삭제
-router.delete("/comments/:comment_id", commentController.deleteComment);
+router.delete(
+  '/comments/:comment_id',
+  validate(commentValidator.deleteComment),
+  commentController.deleteComment,
+);
 
 export default router;
